8605f73db161df18a58a89b0d63f9551d383f953
slrslrr2 <slrslrr1@gmail.com>
2023-04-15 16:34:03 +0900

    Github Actions - Applcation.properties 민감정보 관련 관리하기

--- src/main/java/com/vivid/dream/controller/TestController.java
+++ src/main/java/com/vivid/dream/controller/TestController.java
@@ -6,6 +6,8 @@ import com.vivid.dream.config.handler.exception.WebException;
 import com.vivid.dream.model.SongVo;
 import com.vivid.dream.service.SongService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
+    @Value("${spring.spring.jdbc-url}")
+    String jdbcUrl2;
+
+    @Value("${spring.elasticsearch.host}")
+    String elasticsearchHost;
 
     private final ElasticsearchClient elasticsearchClient;
     private final SongService songService;
 
-    @RequestMapping("/elastic/test")
+    @GetMapping("/elastic/test")
     public void elasticTest() throws IOException {
         if (elasticsearchClient.exists(b -> b.index("products").id("foo")).value()) {
             System.out.println("product exists");
@@ -36,5 +44,10 @@ public class TestController {
             throw new WebException(ResultCode.ERR_REQUEST_PARAMETER);
         }
         return songService.getSongList(date);
+    }
+
+    @GetMapping("/properties")
+    public void propertiesList(){
+        log.info("변수가 찍히고 있다 TEST : =>  {} {} ", jdbcUrl2, elasticsearchHost);
     }
 }
