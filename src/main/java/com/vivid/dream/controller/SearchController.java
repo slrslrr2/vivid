05e37e74b1dcf1d663bbaa891e61b204dc63a96b
slrslrr2 <slrslrr1@gmail.com>
2023-05-11 22:50:06 +0900

    search 로그 쌓기 BackEnd

--- src/main/java/com/vivid/dream/controller/SearchController.java
+++ src/main/java/com/vivid/dream/controller/SearchController.java
@@ -3,12 +3,12 @@ package com.vivid.dream.controller;
 import com.vivid.dream.config.handler.ResultCode;
 import com.vivid.dream.config.handler.exception.WebException;
 import com.vivid.dream.service.SearchService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +18,13 @@ public class SearchController {
     final private SearchService searchService;
 
     @GetMapping("/song/info")
-    public ResponseEntity<?> getSongInfo(HttpServletRequest request){
+    public ResponseEntity<?> getSongInfo(@RequestParam String keyword){
         try{
+            if (!"".equals(keyword)) {
+                System.out.println("keyword: "+ keyword);
+                return null;
+            }
+            // TODO: search queryDSL
             String indexName = searchService.getLastIndexName("song_info");
             System.out.println(indexName);
         }catch (Exception e){
