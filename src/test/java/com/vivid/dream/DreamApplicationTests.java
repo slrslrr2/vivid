4bb76b5b08c5997e976508565a15f74136f639b8
slrslrr2 <slrslrr1@gmail.com>
2023-09-02 20:11:45 +0900

    테스트코드 제외

--- src/test/java/com/vivid/dream/DreamApplicationTests.java
+++ src/test/java/com/vivid/dream/DreamApplicationTests.java
@@ -1,13 +1,12 @@
 package com.vivid.dream;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DreamApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
